16a7cfee37147a96d9aa6c102b6961fa98073d4e
balakrishnan4591 <balakrishnan4591@gmail.com>
2024-10-20 04:13:17 +0530

    added order page functionality, order status update functionality

--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -1,8 +1,92 @@
 import React from "react";
 import "./Orders.css";
+import { useState, useEffect } from "react";
+import { toast } from "react-toastify";
+import axios from "axios";
+import { assets } from "../../assets/assets";
 
-const Orders = () => {
-  return <div>Order</div>;
+const Orders = ({ url }) => {
+  const [orders, setOrders] = useState([]);
+
+  const fetchAllOrders = async () => {
+    const response = await axios.get(url + "/api/order/list");
+    console.log(response.data.success);
+
+    if (response.data.success) {
+      setOrders(response.data.data);
+      console.log(response.data.data);
+
+      console.log(orders);
+    } else {
+      toast.error("Error");
+    }
+  };
+
+  const statusHandler = async (event, orderId) => {
+    const response = await axios.post(url + "/api/order/status", {
+      orderId,
+      status: event.target.value,
+    });
+    if (response.data.success) {
+      await fetchAllOrders();
+    }
+  };
+
+  useEffect(() => {
+    fetchAllOrders();
+  }, []);
+
+  return (
+    <div className="order add">
+      <h3>All Orders</h3>
+      <div className="order-list">
+        {orders.map((order, index) => (
+          <div key={index} className="order-item">
+            <img src={assets.parcel_icon} alt="" />
+            <div>
+              <p className="order-item-service">
+                {order.service.map((item, index) => {
+                  if (index === order.service.length - 1) {
+                    return item.name + " x " + item.quantity;
+                  } else {
+                    return item.name + " x " + item.quantity + ", ";
+                  }
+                })}
+              </p>
+              <p className="order-item-name">
+                {order.address.firstName + " " + order.address.lastName}
+              </p>
+              <div className="order-item-address">
+                <p>
+                  <p>{order.address.street + ","}</p>
+                  <p>
+                    {order.address.city +
+                      ", " +
+                      order.address.state +
+                      ", " +
+                      order.address.country +
+                      ", " +
+                      order.address.zipcode}
+                  </p>
+                </p>
+              </div>
+              <p className="order-item-phone">{order.address.phone}</p>
+            </div>
+            <p>Items: {order.service.length}</p>
+            <p>₹ {order.amount}</p>
+            <select
+              onChange={(event) => statusHandler(event, order._id)}
+              value={order.status}
+            >
+              <option value="Order accepted">Order accepted</option>
+              <option value="Service in progress">Service in-progress</option>
+              <option value="Service completed">Service completed</option>
+            </select>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Orders;
